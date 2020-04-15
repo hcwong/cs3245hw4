9e99e3732cba97c88b9b423a7997d74d006ccbbd
C-E-OOI <e0201208@u.nus.edu>
2020-04-15 14:45:47 +0800

    edit for title and court (in enum field) and docID

--- index.py
+++ index.py
@@ -10,6 +10,7 @@ import csv
 import functools
 from collections import Counter, defaultdict
 from encode import encode
+from enum import IntEnum
 
 # Self-defined constants, functions and classes
 
@@ -40,7 +41,8 @@ class VSM:
     Represents the Vector Space Model
     """
     def __init__(self, in_dir, d_file, p_file):
-        self.dictionary = {}
+        self.dictionary = {}  # content, title, court
+        self.docid_set = set() # docID
         self.in_dir = in_dir
         self.d_file = d_file
         self.p_file = p_file
@@ -53,18 +55,40 @@ class VSM:
         Punctuation handling, tokenisation, case-folding, stemming are applied to generate terms
         """
         # Step 1: Obtain Postings for material to create VSM in Step 3
-        tokens_list = []
         set_of_documents = self.get_documents()
+        tokens_list = [] 
+
         # Save flattened Counter results in tokens_list
         for res in set_of_documents:
+            # Part 1. Process doc_id into a set
             doc_id = res['doc_id']
-            positional_indexes = res['positional_indexes']
-            for term, positions in positional_indexes.items():
-                tokens_list.append([term, (doc_id, positions)])
+            if doc_id.isdigit():
+                if doc_id not in self.docid_set:
+                    self.docid_set.add(doc_id)
+                else:
+                    print("Error: doc_id " + str(doc_id) + " is repeated")
+            else:
+                print("Error: doc_id " + str(doc_id) + " is not digits")
+
+            # Part 2. Process content into tokens of positional indexes (subsequently form into posting list)
+            content_positional_indexes = res['content_positional_indexes']
+            for term, positions in content_positional_indexes.items():
+                tokens_list.append([term, (doc_id, Field.CONTENT, positions)])  # [term, (doc_ID, Field, position]
+            """
+            # Part 3. Process title into tokens of positional indexes (subsequently form into posting list)
+            title_positional_indexes = res['title_positional_indexes']
+            for term, positions in title_positional_indexes.items():
+                tokens_list.append([term, (doc_id, Field.TITLE, positions)])  # [term, (doc_ID, Field, position]
+            
+            # Part 4. Process court into tokens of positional indexes (subsequently form into posting list)
+            court_positional_indexes = res['court_positional_indexes']
+            for term, positions in court_positional_indexes.items():
+                tokens_list.append([term, (doc_id, Field.COURT, positions)])  # [term, (doc_ID, Field, position]  
+            """
         tokens_list.sort(key=functools.cmp_to_key(comparator)) # Sorted list of [term, (doc_id, freq_in_doc)] elements
 
         # Step 2: Get a list of all available doc_ids in ascending order
-        self.doc_ids = sorted(list(set([el['doc_id'] for el in set_of_documents])))
+        #self.doc_ids = sorted(list(set([el['doc_id'] for el in set_of_documents])))
 
         print("Generating posting lists")
 
@@ -73,7 +97,7 @@ class VSM:
         for i in range(len(tokens_list)):
             curr = tokens_list[i]
             term = curr[0]
-            curr_tuple = curr[1] # (doc_id, term frequency, title, court)
+            curr_tuple = curr[1] # (doc_id, Field, freq_in_doc)
             if i == 0 or term != tokens_list[i-1][0]:
                 # new term
                 self.dictionary[term] = PostingList()
@@ -95,6 +119,20 @@ class VSM:
         with open(self.in_dir, encoding='utf-8') as f:
             # prevent csv field larger than field limit error
             csv.field_size_limit(sys.maxsize)
+            """
+            # To run csv.field_size_limit(sys.maxsize) LOCALLY
+            # by resolving "OverflowError: Python int too large to convert to C long" caused by:
+            # TO BE DELETED LTR
+            maxInt = sys.maxsize
+            while True:
+                # decrease the maxInt value by factor 10
+                # as long as the OverflowError occurs.
+                try:
+                    csv.field_size_limit(maxInt)
+                    break
+                except OverflowError:
+                    maxInt = int(maxInt / 10)
+            """
 
             csv_reader = csv.reader(f, delimiter=',')
             documents = []
@@ -106,7 +144,7 @@ class VSM:
                   document['doc_id'] = int(row[0].strip(''))
                   document['title'] = row[1].strip('')
                   document['content'] = row[2].strip('')
-                  document['title'] = row[3].strip('')
+                  document['date_posted'] = row[3].strip('')
                   document['court'] = row[4].strip('')
                   documents.append(document)
                 index += 1
@@ -121,38 +159,67 @@ class VSM:
         documents = self.process_file()
         print("Done processing file")
 
-        # Then we handle the content
-        # For zones, we adopt a broad zoning procedure, with Judgment and non Judgment being the only zone
+        # Then we handle the fields: content, title and court (to generate position index)
+        # 'content_positional_indexes' is made from 'content'
+        # 'title_positional_indexes' is made from 'title'
+        # 'court_positional_indexes' is made from 'court'
         count = 0
         for document in documents:
+            """
             sentences = nltk.sent_tokenize(document['content'])
             words_array = [nltk.word_tokenize(s) for s in sentences]
             words = [w for arr in words_array for w in arr]
             processed_words = self.process_words(words)
             # This is a dictionary of word and the positions it appears in
-            positional_indexes = self.generate_positional_indexes(processed_words, 0)
-            document['positional_indexes'] = positional_indexes
+            content_positional_indexes = self.generate_positional_indexes(processed_words, 0)
+            """
+            document['content_positional_indexes'] = self.generate_positional_indexes(document['content'])  # Part 1: Content
+            document['title_positional_indexes'] = self.generate_positional_indexes(document['title'])  # Part 2: Title
+            document['court_positional_indexes'] = self.generate_positional_indexes(document['court'])  # Part 3: Court
             set_of_documents.append(document)
-            print(count," Generated poisitional index")
+            print(count," Generated positional indexes")
             count += 1
 
         print("Done getting documents")
 
         return set_of_documents
 
-    # This function aims to generate the positional indexes for the phrasal queries
-    def generate_positional_indexes(self, words, start_index):
+    def generate_positional_indexes(self, paragraph):
+        """
+        Generates positional index from a paragraph/string
+        by using the function generate_list_of_words() and generate_positional_indexes_from_list()
+        """
+        processed_words = self.generate_list_of_words(paragraph)
+        # This is a dictionary of word and the positions it appears in
+        positional_indexes = self.generate_positional_indexes_from_list(processed_words, 0)
+        return positional_indexes
+
+    def generate_list_of_words(self, paragraph):
+        """
+        Generates a list of processed words from the string it was input with
+        """
+        sentences = nltk.sent_tokenize(paragraph)
+        words_array = [nltk.word_tokenize(s) for s in sentences]
+        words = [w for arr in words_array for w in arr]
+        processed_words = self.process_words(words)
+        return processed_words
+
+    def generate_positional_indexes_from_list(self, words, start_index):
+        """
+        Generate the positional indexes for the phrasal queries from a list of words
+        Does gap encoding too
+        """
         positions = defaultdict(list)
         last_position = {}
         for i in range(start_index, len(words)):
             word = words[i]
             # Store gap encoding
             if word not in last_position:
-              last_position[word] = i
-              positions[word].append(i)
+                last_position[word] = i
+                positions[word].append(i)
             else:
-              positions[word].append(i - last_position[word])
-              last_position[word] = i
+                positions[word].append(i - last_position[word])
+                last_position[word] = i
         return positions
 
     def process_words(self, words):
@@ -199,14 +266,20 @@ class VSM:
             pickle.dump(d, f) # (term to file cursor value) mappings dictionary
             pickle.dump(self.doc_lengths, f)
 
+class Field(IntEnum):
+    CONTENT = 1
+    TITLE = 2
+    COURT = 3
+
 class Posting:
     """
     Each Posting has a document id doc_id, term frequency freq,
     and weight which is its lnc calculation before normalisation
     """
-    def __init__(self, index, doc_id, positions):
+    def __init__(self, index, doc_id, field, positions):
         self.doc_id = doc_id
         self.positions = positions
+        self.field = field
         self.pointer = None
 
     def var_byte_encoding(self):
@@ -225,16 +298,16 @@ class PostingList:
         return self.size
 
     # Insert with var byte encoding
-    def insert(self, doc_id, positions):
+    def insert(self, doc_id, field, positions):
         next_id = self.size
-        new_posting = Posting(next_id, doc_id, positions)
+        new_posting = Posting(next_id, doc_id, field, positions)
         new_posting.var_byte_encoding()
         self.postings.append(new_posting)
         self.size += 1
 
-    def insert_without_encoding(self, doc_id, positions):
+    def insert_without_encoding(self, doc_id, field, positions):
         next_id = self.size
-        self.postings.append(Posting(next_id, doc_id, positions))
+        self.postings.append(Posting(next_id, doc_id, field, positions))
         self.size += 1
 
     def insert_posting(self, posting):
